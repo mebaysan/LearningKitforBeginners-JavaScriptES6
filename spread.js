a1bc679699f5617a793dbffd0f53c54248f53179
mebaysan <menesbaysan@gmail.com>
2019-05-16 13:22:41 +0300

    spread operation

---
+++ spread.js
@@ -0,0 +1,8 @@
+// Spread Operator
+
+const langs = ["Python", "C#", "JavaScript"];
+
+console.log(langs[0], langs[1], langs[2]);
+
+console.log(...langs); // spread operatör sayesinde yukarıdakinin aynısını yapmış olduk.
+
