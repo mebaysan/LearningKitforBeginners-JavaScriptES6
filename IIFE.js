d1ab591f2121283fabbff512c0742caeeb2602e3
mebaysan <menesbaysan@gmail.com>
2019-08-27 23:55:18 +0300

    IIFE

---
+++ IIFE.js
@@ -0,0 +1,7 @@
+// Immediately Invoke Function Expression
+
+// tanımlandığı yerde çalışan fonksiyon demektir.
+
+(function (name) { // burda fonksiyonu tanımladık
+    console.log("Merhaba " + name)
+})("Baysan"); // ve direk çağırdık
