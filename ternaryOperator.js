6b2a631f0775b161912467c5b153037d0765faca
mebaysan <menesbaysan@gmail.com>
2019-08-28 15:05:59 +0300

    ternary operator

---
+++ ternaryOperator.js
@@ -0,0 +1,7 @@
+function topla(a, b) {
+    return a + b;
+}
+
+var tahmin = 10;
+console.log(topla(2, 4) == 100 ? "tebrikler doğru tahmin " : "Malesef yanlış tahmin");
+//   koşul ? doğruysa : değilse
