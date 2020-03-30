1b752a7fef4877489ca6315ff8fb2541cd904840
mebaysan <menesbaysan@gmail.com>
2020-03-30 03:58:16 +0300

    start the JavaScript bootcamp

---
+++ Modern_JavaScript_Bootcamp/1-)Basics_Variables&FlowControl/1-)String&Variables.js
@@ -0,0 +1,5 @@
+let first_name = 'Enes';
+let last_name = 'Baysan';
+let full_name = `${first_name} ${last_name}`; // bu şekilde string içerisinde js değişkenlerimizi kullanabiliriz
+// console.log(first_name + ' ' + last_name);
+console.log(full_name);
